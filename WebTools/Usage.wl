3bd9b36876de0493995ea0e290ace406f6369fe0
Arnoud Buzing <arnoudb@wolfram.com>
2018-03-21 10:06:43 -0500

    LaunchWebDriver -> LaunchDriver move usage to Usage.wl

---
+++ WebTools/Usage.wl
@@ -0,0 +1,62 @@
+LaunchDriver::usage = "LaunchDriver[] launches the default web driver which allows Mathematica to communicate with a web browser.\n\
+LaunchDriver[driver] launches the specified driver.\n\
+LaunchDriver[\"Chrome\"] launches the driver for Chrome.\n\
+LaunchDriver[\"Firefox\"] launches the driver for Firefox.\n\
+LaunchDriver[\"Edge\"] launches the driver for Edge.";
+
+
+$wtSupportedWebDrivers::usage = "$wtSupportedWebDrivers returns the list of web drivers supported on your platform.";
+
+(* Web session functions *)
+
+wtStartWebSession::usage = "wtStartWebSession[] starts a new web session in a web browser and returns the unique session identifier.";
+wtStopWebSession::usage = "wtStopWebSession[sessionid] stops the web session identified by 'sessionid'";
+wtWebSessionStatus::usage = "wtWebSessionStatus[] returns status information for the current web session. wtWebSessionStatus[sessionid] returns status information for the specified 'sessionid'";
+$wtCurrentWebSession::usage = "$wtCurrentWebSession holds the session identifier for the current web session.";
+$wtWebSessions::usage = "$wtWebSessions returns the list of all open web sessions.";
+
+
+wtOpenWebPage::usage = "wtOpenWebPage[url] opens the web page specified by 'url'";
+wtRefreshWebPage::usage = "wtRefreshWebPage[] refreshes the currently open web page.";
+wtPageBack::usage = "wtPageBack[] instructs the web browser to navigate to the previously visible page in its web session.";
+wtPageForward::usage = "wtPageForward[] instructs the web browser to navigate to the next visible page in its web session and acts as the inverse operation of wtPageBack[].";
+wtPageLinks::usage = "wtPageLinks[] returns the hyperlinks for the current active page.";
+
+wtCaptureWebPage::usage = "wtCaptureWebPage[]";
+wtSetBrowserWindow::usage = "wtSetBrowserWindow[windowid] changes the active browser window to the one specified by 'windowid'. A list of all window identifiers is given by wtBrowserWindows[].";
+wtBrowserWindows::usage = "wtBrowserWindows[] returns a list of all window identifiers, that are open in a given web session.";
+
+wtJavascriptExecute::usage = "wtJavascriptExecute[\"javascript\"] executes a piece of JavaScript in the current window";
+wtLocateElement::usage = "wtLocateElement[id] returns";
+wtClickElement::usage = "wtClickElement[id] clicks on the first web element identified by 'id'. wtClickElement[{id,n}] clicks the n-th matching element identified by 'id'. Typical elements are form submit buttons and hyperlinks.";
+wtTypeElement::usage = "wtTypeElement[id, text] types 'text' into a web element identified by 'id'. Typical elements are input fields and text areas.";
+wtClearElement::usage = "wtClearElement[id] clears text of a web elements identified by 'id'. Typical elements are input field and text areas.";
+wtSubmitElement::usage = "";
+wtHoverElement::usage = "wtHoverElement[id] clicks on the first web element identified by 'id'. wtClickElement[{id,n}] clicks the n-th matching element identified by 'id'. Typical elements are form submit buttons and hyperlinks.";
+wtHideElement::usage = "";
+
+wtFocusFrame::usage = "wtFocusFrame[id] gives focus to a web frame inside a web page. Typical usage is for giving focus to elements within <iframe> tags"
+
+wtElementClassName::usage = "";
+wtCssSelector::usage = "";
+wtId::usage = "";
+wtName::usage = "";
+wtLinkText::usage = "";
+wtPartialLinkText::usage = "";
+wtTagName::usage = "";
+wtXPath::usage = "";
+
+wtSelector::usage="";
+
+$wtWebDriver::usage = "";
+$wtWebDriverBaseURL::usage = "";
+(* Javascript based functions *)
+
+wtGetPageHtml::usage = "Get HTML of current tab.";
+
+wtGetHtml::usage = "Get part of HTML of current tab, search method include wtId[], wtXPath and wtSelector[].
+Option \"Seletion\" could be set to \"inner\" or \"outer\" which control the returned part of the selected HTML segment.";
+
+wtGetPageURL::usage = "Get URL of current tab.";
+
+wtOffAlert::usage = "wtOffAlert[] would turn off all bump up windows including alerts(), confirm() and prompt() and automatically confirm them."
--- WebTools/WebTools.m
+++ WebTools/WebTools.m
@@ -1,67 +1,6 @@
 BeginPackage["WebTools`", {"CloudObject`", "CURLLink`"}];
 
-LaunchWebDriver::usage = "LaunchWebDriver[] launches the default web driver which allows Mathematica to communicate with a web browser.\n\
-LaunchWebDriver[driver] launches the specified driver.\n\
-LaunchWebDriver["Chrome"] launches the driver for Chrome.\n\
-LaunchWebDriver["Firefox"] launches the driver for Firefox.\n\
-LaunchWebDriver["Edge"] launches the driver for Edge.";
-
-
-$wtSupportedWebDrivers::usage = "$wtSupportedWebDrivers returns the list of web drivers supported on your platform.";
-
-(* Web session functions *)
-
-wtStartWebSession::usage = "wtStartWebSession[] starts a new web session in a web browser and returns the unique session identifier.";
-wtStopWebSession::usage = "wtStopWebSession[sessionid] stops the web session identified by 'sessionid'";
-wtWebSessionStatus::usage = "wtWebSessionStatus[] returns status information for the current web session. wtWebSessionStatus[sessionid] returns status information for the specified 'sessionid'";
-$wtCurrentWebSession::usage = "$wtCurrentWebSession holds the session identifier for the current web session.";
-$wtWebSessions::usage = "$wtWebSessions returns the list of all open web sessions.";
-
-
-wtOpenWebPage::usage = "wtOpenWebPage[url] opens the web page specified by 'url'";
-wtRefreshWebPage::usage = "wtRefreshWebPage[] refreshes the currently open web page.";
-wtPageBack::usage = "wtPageBack[] instructs the web browser to navigate to the previously visible page in its web session.";
-wtPageForward::usage = "wtPageForward[] instructs the web browser to navigate to the next visible page in its web session and acts as the inverse operation of wtPageBack[].";
-wtPageLinks::usage = "wtPageLinks[] returns the hyperlinks for the current active page.";
-
-wtCaptureWebPage::usage = "wtCaptureWebPage[]";
-wtSetBrowserWindow::usage = "wtSetBrowserWindow[windowid] changes the active browser window to the one specified by 'windowid'. A list of all window identifiers is given by wtBrowserWindows[].";
-wtBrowserWindows::usage = "wtBrowserWindows[] returns a list of all window identifiers, that are open in a given web session.";
-
-wtJavascriptExecute::usage = "wtJavascriptExecute[\"javascript\"] executes a piece of JavaScript in the current window";
-wtLocateElement::usage = "wtLocateElement[id] returns";
-wtClickElement::usage = "wtClickElement[id] clicks on the first web element identified by 'id'. wtClickElement[{id,n}] clicks the n-th matching element identified by 'id'. Typical elements are form submit buttons and hyperlinks.";
-wtTypeElement::usage = "wtTypeElement[id, text] types 'text' into a web element identified by 'id'. Typical elements are input fields and text areas.";
-wtClearElement::usage = "wtClearElement[id] clears text of a web elements identified by 'id'. Typical elements are input field and text areas.";
-wtSubmitElement::usage = "";
-wtHoverElement::usage = "wtHoverElement[id] clicks on the first web element identified by 'id'. wtClickElement[{id,n}] clicks the n-th matching element identified by 'id'. Typical elements are form submit buttons and hyperlinks.";
-wtHideElement::usage = "";
-
-wtFocusFrame::usage = "wtFocusFrame[id] gives focus to a web frame inside a web page. Typical usage is for giving focus to elements within <iframe> tags"
-
-wtElementClassName::usage = "";
-wtCssSelector::usage = "";
-wtId::usage = "";
-wtName::usage = "";
-wtLinkText::usage = "";
-wtPartialLinkText::usage = "";
-wtTagName::usage = "";
-wtXPath::usage = "";
-
-wtSelector::usage="";
-
-$wtWebDriver::usage = "";
-$wtWebDriverBaseURL::usage = "";
-(* Javascript based functions *)
-
-wtGetPageHtml::usage = "Get HTML of current tab.";
-
-wtGetHtml::usage = "Get part of HTML of current tab, search method include wtId[], wtXPath and wtSelector[].
-Option \"Seletion\" could be set to \"inner\" or \"outer\" which control the returned part of the selected HTML segment.";
-
-wtGetPageURL::usage = "Get URL of current tab.";
-
-wtOffAlert::usage = "wtOffAlert[] would turn off all bump up windows including alerts(), confirm() and prompt() and automatically confirm them."
+Get[ FileNameJoin[{DirectoryName[$InputFileName], "Usage.wl"}] ];
 
 Begin["`Private`"];
 
@@ -103,9 +42,9 @@ randomport[] := Module[{sock,port},
 	port
 	]
 (* execute once to start the standalone driver *)
-LaunchWebDriver[] := LaunchWebDriver["ChromeDriver"];
+LaunchDriver[] := LaunchDriver["ChromeDriver"];
 
-LaunchWebDriver[driver_] := Module[{dir},
+LaunchDriver[driver_] := Module[{dir},
 	{$wtWebDriver,$wtWebDriverBaseURL} = Switch[ driver,
 		"ChromeDriver", {"Chrome","http://localhost:9515"},
 		"InternetExplorerDriver", {"InternetExplorer","http://localhost:5555"},
